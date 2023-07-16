2538e9c234a9da026bfd9fc807d6d499fff4f4b9
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2023-07-16 19:04:33 -0400

    added slider widget and multiselect option for genre filtering, also clear filter button which has some issues.

--- app.py
+++ app.py
@@ -3,6 +3,47 @@ import requests
 from config import Config
 
 
+def get_genres():
+    url = "https://api.themoviedb.org/3/genre/movie/list?language=en-US"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {Config.API_KEY}"
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data['genres']
+
+
+def get_movies(filter_type, release_year=None, genres=None, search_query=None, vote_average_range=None):
+    filters = {
+        'Popular': 'popular',
+        'Now Playing': 'now_playing',
+        'Top Rated': 'top_rated',
+        'Upcoming': 'upcoming'
+    }
+
+    genre_ids = [genre['id'] for genre in genres] if genres else None
+    genre_query = f'&with_genres={",".join(str(id) for id in genre_ids)}' if genre_ids else ''
+
+    if search_query:
+        url = f"https://api.themoviedb.org/3/search/movie?language=en-US&query={search_query}&page=1"
+    else:
+        url = f"https://api.themoviedb.org/3/movie/{filters[filter_type]}?language=en-US&page=1{genre_query}"
+        if release_year:
+            url += f"&year={release_year}"  # Add release year as a filter
+        if vote_average_range:
+            url += f"&vote_average.gte={vote_average_range[0]}&vote_average.lte={vote_average_range[1]}"  # Add vote average range as a filter
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {Config.API_KEY}"
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data['results']
+
+
 def main():
     st.title('Reel Charts')
 
@@ -10,43 +51,38 @@ def main():
     # Sidebar options
     selected_option = st.sidebar.selectbox("Select a chart", ["Home", "Box Office", "Upcoming Movies"])
 
-    def get_movies(filter_type, release_year=None, search_query=None):
-        filters = {
-            'Popular': 'popular',
-            'Now Playing': 'now_playing',
-            'Top Rated': 'top_rated',
-            'Upcoming': 'upcoming'
-        }
-
-        if search_query:
-            url = f"https://api.themoviedb.org/3/search/movie?language=en-US&query={search_query}&page=1"
-        else:
-            url = f"https://api.themoviedb.org/3/movie/{filters[filter_type]}?language=en-US&page=1"
-
-        headers = {
-            "accept": "application/json",
-            "Authorization": f"Bearer {Config.API_KEY}"
-        }
-
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        return data['results']
+    def get_selected_genres():
+        all_genres = get_genres()
+        genre_names = [genre['name'] for genre in all_genres]
+        selected_genres = st.sidebar.multiselect('Select Genres', genre_names)
+        selected_genre_ids = [genre['id'] for genre in all_genres if genre['name'] in selected_genres]
+        selected_genres = [{'id': genre_id, 'name': genre_name} for genre_id, genre_name in
+                           zip(selected_genre_ids, selected_genres)]
+        return selected_genres
 
     # home page filters
     if selected_option == 'Home':
         filter_type = st.radio('What movies would you like to see?',
                                ['Popular', 'Now Playing', 'Top Rated', 'Upcoming'])
         search_query = st.text_input("Search by movie title")
+        release_year = st.number_input("Filter by release year", min_value=1990, max_value=2023, step=1,
+                                       value=2023)  # Set default value to 2023
+        vote_average_range = st.slider("Filter by vote average range", min_value=0.0, max_value=10.0,
+                                       value=(0.0, 10.0), step=0.1)  # Slider widget for vote average range
 
-        # Clear release_year input if a search query is entered
-        if search_query:
-            release_year = None
-        else:
-            release_year = st.number_input("Filter by release year", min_value=1990, max_value=2023, step=1)
+        genres = get_selected_genres()
+        movies = get_movies(filter_type, release_year, genres, search_query, vote_average_range)
 
-        movies = get_movies(filter_type, release_year, search_query)
+        # Dynamically change the header based on the selected filter type
+        header_text = filter_type + " Movies"
+        st.header(header_text)
 
-        st.header('Popular Movies')
+        # Clear Filters button
+        if st.button("Clear Filters"):
+            search_query = None
+            release_year = 2023
+            vote_average_range = (0.0, 10.0)
+            genres = None
 
         columns = st.columns(3)
 
