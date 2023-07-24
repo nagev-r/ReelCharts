9d758aaed23815391099cac44e7b53263fd1bfef
ChildishTurbino <Zacharyriveracshs@gmail.com>
2023-07-24 18:46:43 -0400

    Added the Line Chart

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd  # Gio to implente tables feature
 import numpy as np
 from config import Config
+import pandas as pd
 
 
 def get_genres():
@@ -15,6 +16,15 @@ def get_genres():
     data = response.json()
     return data['genres']
 
+def get_box_office(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?language=en-US"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {Config.API_KEY}"
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data.get('revenue', 0)
 
 def get_movies(filter_type, release_year=None, genres=None, search_query=None, vote_average_range=None):
     filters = {
@@ -51,7 +61,7 @@ def main():
 
     st.sidebar.title('Charts')
     # Sidebar options
-    selected_option = st.sidebar.selectbox("Select a chart", ["Home", "Box Office", "Upcoming Movies", "Movie Table"])
+    selected_option = st.sidebar.selectbox("Select a chart", ["Home", "Movie Table", "Popularity Chart","Box Office Line Graph"])
 
     def get_selected_genres():
         all_genres = get_genres()
@@ -97,6 +107,68 @@ def main():
                 st.image(poster_url, width=200)
                 st.write(movie['title'])
 
+    # Zach Code Start
+    if selected_option == 'Popularity Chart':
+        movies = get_movies('Popular')
+        if movies:
+            # Create horizontal bar chart
+            movie_titles = [movie['title'] for movie in movies]
+            movie_popularity = [movie['popularity'] for movie in movies]
+            # Rescale popularity to a range of 1 to 10
+            min_popularity = min(movie_popularity)
+            max_popularity = max(movie_popularity)
+            rescaled_popularity = [
+                1 + (popularity - min_popularity) * 9 / (max_popularity - min_popularity)
+                for popularity in movie_popularity
+            ]
+            chart_data = pd.DataFrame({'Movie': movie_titles, 'Popularity': rescaled_popularity})
+            chart_data = chart_data.nlargest(10, 'Popularity')  # Limit to top 10 movies
+            chart_data = chart_data.sort_values('Popularity', ascending=True)  # Sort in ascending order
+            chart_data = chart_data[::-1]  # Reverse the order of rows
+            st.header('Top 10 Current Movies by Popularity')
+
+            # Display horizontal bar chart
+            for index, row in chart_data.iterrows():
+                movie_info = st.empty()
+                popularity_bar = st.empty()
+                popularity_text = st.empty()
+                movie_info.text(f"{row['Movie']} ({row['Popularity']: .1f} / 10.0)")
+                popularity_bar.progress(row['Popularity'] / 10.0)
+
+    if selected_option == 'Box Office Line Graph':
+        if selected_option == 'Box Office Line Graph':
+            st.header('Box Office Income for 5 Movies (Month of July)')
+            st.write(
+                'Movies: Spider-Man: Across the Spider-Verse, Elemental, Transformers: Rise of the Beasts, Guardians of the Galaxy Vol. 3, Indiana Jones and the Dial of Destiny')
+
+            # Simulated box office data for the 5 movies for the last month
+            # Replace these with actual box office incomes or data fetching logic
+            movie_incomes = [
+                [340377908, 357678700, 368800269, 375209269],  # Movie 1 weekly incomes
+                [89607297, 109624302, 125688508, 137157319],  # Movie 2 weekly incomes
+                [136463958, 146829334, 152785930, 155664410],  # Movie 3 weekly incomes
+                [355096603, 357570077, 358488599, 358826794],  # Movie 4 weekly incomes
+                [60368101, 122126405, 145628290, 158997363],  # Movie 5 weekly incomes
+             ]
+
+            # Weekly intervals for the last month
+            weeks = ['Week 1', 'Week 2', 'Week 3', 'Week 4']
+
+            movie_titles = ['Spider-Man: Across the Spider-Verse', 'Elemental', 'Transformers: Rise of the Beasts',
+                            'Guardians of the Galaxy Vol. 3', 'Indiana Jones and the Dial of Destiny']
+
+            # Create a dataframe for the line chart
+            data = pd.DataFrame({
+                'Week': weeks,
+                **{title: incomes for title, incomes in zip(movie_titles, movie_incomes)}
+            })
+
+            # Set the 'Week' column as the index for the dataframe
+            data.set_index('Week', inplace=True)
+
+            # Display the line chart using Streamlit
+            st.line_chart(data)
+    # Zach Code End
 
     # Gio code start
     if selected_option == "Movie Table":
